9d7a108696440e9889d945db1f82f469b8858288
Luis Yps Fdez <lueryefe@gmail.com>
2022-03-08 12:56:06 -0500

    Fixing visualization issue of dinamic Fibonacci's Serie

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ const time = document.getElementById('time');
 time.innerHTML = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
 
 const calcularFibonacci = () => {
-    // location.reload(true);
     let inputSeed = today.getMinutes();
-    let lastPush = today.getSeconds();
-    
+    let lastPush = today.getSeconds(); 
     let seedArray = String(inputSeed).split('').map(item => Number(item));
 
     if (seedArray.length == 1) {
         seedArray.unshift(0);
-        console.log(seedArray);
     };
     
     for (let i = 0; i < lastPush; i++) {  
@@ -20,18 +17,11 @@ const calcularFibonacci = () => {
         let newPush = element1 + element2;
         seedArray.push(newPush);
     };
-    console.log(seedArray);
 
     const result = document.getElementById('result');
-    result.innerHTML = "La serie de Fibonacci es: " + seedArray ; // + "<br>" + seedArray
+    result.innerHTML = "La serie de Fibonacci es: "; // + "<br>" + seedArray
 
-    // for (let i = 0; i < seedArray.length; i++) {
-    //     result.innerHTML = `${seedArray[i]}, `
-    //     if (i > 20) {
-    //         result.innerHTML = `\n ${seedArray[i]}, `;
-    //     } else if (i > 40) {
-    //         result.innerHTML = `\n ${seedArray[i]}, `;
-    //     }
-    // }
+    for (let i = 0; i < seedArray.length; i++) {
+        result.innerHTML += `${seedArray[i]}, `;
+    }
 }
-
