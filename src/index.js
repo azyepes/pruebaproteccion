fde98d8b6b39a82b82a803908469fde6acffa62f
Luis Yps Fdez <lueryefe@gmail.com>
2022-03-08 10:53:28 -0500

    Nuevo commit 3

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ time.innerHTML = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()
 const calcularFibonacci = () => {
     // location.reload(true);
     let inputSeed = today.getMinutes();
-    console.log(typeof(inputSeed));
     let lastPush = today.getSeconds();
     
     let seedArray = String(inputSeed).split('').map(item => Number(item));
-    console.log(seedArray);
 
     if (seedArray.length == 1) {
         seedArray.unshift(0);
@@ -25,7 +23,15 @@ const calcularFibonacci = () => {
     console.log(seedArray);
 
     const result = document.getElementById('result');
-    result.innerHTML = `La serie de Fibonacci es:
-    ${seedArray}`
+    result.innerHTML = "La serie de Fibonacci es: " + "<br>" + seedArray;
+
+    for (let i = 0; i < seedArray.length; i++) {
+        result.innerHTML = `${seedArray[i]}, `
+        if (i > 20) {
+            result.innerHTML = `\n ${seedArray[i]}, `;
+        } else if (i > 40) {
+            result.innerHTML = `\n ${seedArray[i]}, `;
+        }
+    }
 }
 
